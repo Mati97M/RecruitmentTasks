c3f9511f4becd45b90b6d487de177f9d30c37ea5
Mateusz Mieckowski <MMieckowski@wp.pl>
2023-10-12 10:48:22 +0200

    another solved LeetCode problems LCAofBinarySearchTree  - lowest Common Ancestor of BST solution improved - memory complexity O(1), time: O(log(n))

--- src/main/java/dev/mati/tasks/LeetCode/Trees/LCAofBinarySearchTree.java
+++ src/main/java/dev/mati/tasks/LeetCode/Trees/LCAofBinarySearchTree.java
@@ -26,6 +26,19 @@ public class LCAofBinarySearchTree {
 
     }
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode curr = root;
+        while(true) {
+            if(curr.val == p.val || curr.val == q.val)
+                return curr;
+            if(curr.val > p.val && curr.val > q.val)
+                curr = curr.left;
+            else if(curr.val < p.val && curr.val < q.val)
+                curr = curr.right;
+            else
+                return curr;
+        }
+    }
+    public static TreeNode lowestCommonAncestorMemCostly(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode result = root;
         HashSet<Integer> vals = new HashSet<>();
 
